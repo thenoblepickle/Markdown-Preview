aa726b7d5a4dcf1d7282a213413547607f3f9378
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2026-01-14 18:55:03 +0100

    Working now :)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { marked } from 'marked';
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     const editor = document.getElementById("editor");
@@ -12,21 +12,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     editor.addEventListener("input", updatePreview);
     const defaultMarkdown = `
-    # Heading 1
-    ## Heading 2
-    [Link](https://www.example.com/)
-    \`inline code\`
-    \`\`\`
-    // Code block
-    const greeting = 'Hello';
-    console.log(greeting);
-    \`\`\`
-    - List item 1
-    - List item 2
-    > Blockquote
-    ![Image](https://via.placeholder.com/150)
-    **Bold text**
-        `;
+# Heading 1
+## Heading 2
+[Link](https://developer.mozilla.org/en-US/)
+\`inline code\`
+\`\`\`
+// Code block
+const greeting = 'Hello';
+console.log(greeting);
+\`\`\`
+- List item 1
+- List item 2
+> Blockquote
+![Image](https://i.pinimg.com/1200x/f5/43/c8/f543c8feeb153f5646e04f1d850dade3.jpg)
+**Bold text**
+    `;
 
     editor.value = defaultMarkdown;
     updatePreview();
